0445b1612516aac0113ef930939738a5617d8312
igor <ilaborie@gmail.com>
2020-04-20 19:30:58 +0200

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
 mod utils;
+mod canvas;
 
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use web_sys::HtmlCanvasElement;
+use crate::canvas::draw_hello;
+use crate::utils::set_panic_hook;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -9,11 +14,15 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
-}
+pub fn attach_canvas(canvas_id: &str) {
+    set_panic_hook(); // FIXME remove later
 
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, plop-mario-wasm!");
+    let document = web_sys::window().unwrap().document().unwrap();
+    let canvas = document.get_element_by_id(canvas_id).unwrap();
+    let canvas: HtmlCanvasElement = canvas
+        .dyn_into::<HtmlCanvasElement>()
+        .map_err(|_| ())
+        .unwrap();
+
+    draw_hello(canvas)
 }
